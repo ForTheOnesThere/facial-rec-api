ac5d10803561d2850c27f3ee788ca474a89486c5
ForTheOnesThere <jamescockayne1@gmail.com>
2020-12-24 21:10:13 +0000

    Added sign in and register end points

--- server.js
+++ server.js
@@ -1,25 +1,73 @@
 //Packages
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'test',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Hannah',
+            email: 'hannah@gmail.com',
+            password: 'test',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
 //Middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 //
 app.get('/', (req, res) => {
-    res.send('working!');
+    console.log('success');
+    res.send(database.users);
+});
+
+app.post('/signin', (req,res) => {
+    if (req.body.email === database.users[0].email &&  req.body.password === database.users[0].password){
+        console.log(database.users[0].password);
+        res.json('this is working!') 
+    } else {
+        res.status(400).json('error logging in');
+        console.log(req.body)
+    } 
+});
+
+app.post('/register', (req,res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,   
+        entries: 0,  
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length -1])
 })
+
 
 
 //Start listening
 app.listen(3000, () => {
     console.log('App running on port 3000')
-})
+});
 
 /*
-/ => working
+/ => user list
 /signin => POST, success/fail
 /register => POST, return user
 /profile/:userid => GET user
